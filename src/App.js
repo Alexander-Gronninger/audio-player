30ae9e1cdbcce2fb3203332df074e805380eb3a0
Alexander-Gronninger <alexander.gronninger@gmail.com>
2022-09-08 13:01:07 +0200

    add package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AudioPlayer autoplay src="https://example.com/audio.mp3" />
+    </div>
+  );
 }
 
 export default App;
